8385c8defa4784c75f5d4041ac78e7f42a408754
Suyeon Hong <bellahong95@gmail.com>
2022-07-29 19:48:34 +0900

    register route 추가

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const mongoose = require("mongoose");
 mongoose
 	.connect(
@@ -12,6 +18,17 @@ mongoose
 
 app.get("/", (req, res) => {
 	res.send("Hello World! 안녕하세요~~~");
+});
+
+app.post("/register", (req, res) => {
+	const user = new User(req.body);
+
+	user.save((err, userInfo) => {
+		if (err) return res.json({ success: false, err });
+		return res.status(200).json({
+			success: true,
+		});
+	});
 });
 
 app.listen(port, () => {
