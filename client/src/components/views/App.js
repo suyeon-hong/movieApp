f6394b535df4465d6fda8713ff316561921c2b59
SuyeonHong <bellahong95@gmail.com>
2022-08-09 22:42:14 +0900

    헤더 NavBar 추가

--- client/src/components/views/App.js
+++ client/src/components/views/App.js
@@ -1,28 +1,41 @@
 import React from "react";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { LandingPage, LoginPage, RegisterPage, MovieDetail } from "./index";
+import {
+	LandingPage,
+	LoginPage,
+	RegisterPage,
+	MovieDetail,
+	NavBar,
+} from "./index";
 
 import Auth from "../../hoc/auth";
 
 function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route exact path="/" element={Auth(LandingPage, null)}></Route>
-				<Route exact path="/LoginPage" element={Auth(LoginPage, false)}></Route>
-				<Route
-					exact
-					path="/RegisterPage"
-					element={Auth(RegisterPage, false)}
-				></Route>
-				<Route
-					exact
-					path="/movie/:movieId"
-					element={Auth(MovieDetail, false)}
-				></Route>
-			</Routes>
-		</Router>
+		<>
+			<Router>
+				<NavBar />
+				<Routes>
+					<Route exact path="/" element={Auth(LandingPage, null)}></Route>
+					<Route
+						exact
+						path="/LoginPage"
+						element={Auth(LoginPage, false)}
+					></Route>
+					<Route
+						exact
+						path="/RegisterPage"
+						element={Auth(RegisterPage, false)}
+					></Route>
+					<Route
+						exact
+						path="/movie/:movieId"
+						element={Auth(MovieDetail, null)}
+					></Route>
+				</Routes>
+			</Router>
+		</>
 	);
 }
 
--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -1,5 +1,115 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "antd";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function NavBar() {
-	return <div>NavBar</div>;
+	const navigate = useNavigate();
+	const [user, setUser] = useState(null);
+	const authenticated = user != null;
+
+	const toMainPage = () => {
+		navigate("/");
+	};
+
+	const toLoginPage = () => {
+		navigate("/LoginPage");
+	};
+
+	const toRegisterPage = () => {
+		navigate("/RegisterPage");
+	};
+
+	const onLogoutHandler = () => {
+		axios.get("/api/users/logout").then((response) => {
+			if (response.data.success) {
+				navigate("/LoginPage");
+			} else {
+				alert("로그아웃에 실패했습니다.");
+			}
+		});
+	};
+
+	// useEffect(() => {
+	// 	async function getUserState() {
+	// 		const result = await axios
+	// 			.get("/api/users/auth")
+	// 			.then((response) => response.data.isAuth);
+	// 		return result;
+	// 	}
+	// 	setUserLogin(getUserState());
+	// }, []);
+
+	return (
+		<header
+			style={{
+				position: "fixed",
+				top: "0",
+				left: "0",
+				width: "100%",
+				height: "50px",
+				background: "#fff",
+				display: "flex",
+				justifyContent: "space-between",
+				alignItems: "center",
+				padding: "0 7%",
+			}}
+		>
+			<h1
+				style={{
+					fontSize: "18px",
+					fontWeight: "bold",
+					letterSpacing: "-.5px",
+					marginBottom: "0px",
+					cursor: "pointer",
+				}}
+				onClick={toMainPage}
+			>
+				MovieApp
+			</h1>
+			<div>
+				{/* {authenticated ? (
+					<Button
+						style={{ height: "100%", border: "none" }}
+						onClick={onLogoutHandler}
+					>
+						Logout
+					</Button>
+				) : (
+					<>
+						<Button
+							style={{ height: "100%", border: "none" }}
+							onClick={toLoginPage}
+						>
+							Login
+						</Button>
+						<Button
+							style={{ height: "100%", border: "none" }}
+							onClick={toRegisterPage}
+						>
+							Register
+						</Button>
+					</>
+				)} */}
+				<Button
+					style={{ height: "100%", border: "none" }}
+					onClick={onLogoutHandler}
+				>
+					Logout
+				</Button>
+				<Button
+					style={{ height: "100%", border: "none" }}
+					onClick={toLoginPage}
+				>
+					Login
+				</Button>
+				<Button
+					style={{ height: "100%", border: "none" }}
+					onClick={toRegisterPage}
+				>
+					Register
+				</Button>
+			</div>
+		</header>
+	);
 }
